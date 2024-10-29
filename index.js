4e9a795cc68741b492525fa1d490d220ff38be81
Adilet Soronov <74559101+adiletelf@users.noreply.github.com>
2024-10-30 01:06:32 +0600

    Refactor code into functions to make it more readable

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-import * as fs from "fs";
-import {parse, html} from "diff2html";
+import { readFileSync, writeFileSync } from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import {
-    lightGitHubTheme,
-    darkGitHubTheme,
-    autoGitHubTheme,
-    lightBaseStyle,
-    darkBaseStyle,
-    autoBaseStyle,
-} from "./styles.js";
+
+import {parse, html} from "diff2html";
+import { autoGitHubTheme, autoBaseStyle } from "./styles.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-function replaceExactly(value, searchValue, replaceValue) {
-    return value.replace(searchValue, () => replaceValue);
-}
-
-const diffString = fs.readFileSync("git.diff", "utf8");
-
-const colorScheme = 'auto';
-const lineByLineParsed = parse(diffString, { outputFormat: "line-by-line", colorScheme });
-const lineByLineHTMLContent = html(lineByLineParsed, { outputFormat: "line-by-line", colorScheme });
-
-const sideBySideParsed = parse(diffString, { outputFormat: "side-by-side", colorScheme });
-const sideBySideHTMLContent = html(sideBySideParsed, { outputFormat: "side-by-side", colorScheme });
-
-const template = fs.readFileSync("template.html", "utf8");
-const cssContent = fs.readFileSync(path.resolve(__dirname, "node_modules/diff2html/bundles/css/diff2html.min.css"), "utf8");
-const slimUIContent = fs.readFileSync(path.resolve(__dirname, "node_modules/diff2html/bundles/js/diff2html-ui-slim.min.js"), "utf8");
-
-
+// 1. Read the arguments
+const diffString = readDiffString();
 const previousVersions = [
     "1.0.11.0,f7be17e,Rejected",
     "1.2.3.0,b3f9dd2,Approved",
@@ -40,28 +18,52 @@ const previousVersions = [
 ];
 const args = previousVersions.join(";");
 const versions = args.split(';');
-
 const versionsHTMLContent = versions.map((version) => `<span>${version.replaceAll(',', ' - ')}</span>`).join('<br/>');
-
 const pageTitle = "Charticulator Visual Community (View) [Individual Entrepreneur Ilfat Galiev]";
-const gitHubTheme = colorScheme === 'light' ? lightGitHubTheme : colorScheme === 'dark' ? darkGitHubTheme : autoGitHubTheme;
-const baseStyle = colorScheme === 'light' ? lightBaseStyle : colorScheme === 'dark' ? darkBaseStyle : autoBaseStyle;
 
-const result = [
-    { searchValue: '<!--diff2html-title-->', replaceValue: pageTitle },
-    { searchValue: '<!--diff2html-header-->', replaceValue: pageTitle },
-    { searchValue: '<!--diff2html-versions-->', replaceValue: versionsHTMLContent },
-    {
-        searchValue: '<!--diff2html-css-->',
-        replaceValue: `${baseStyle}\n${gitHubTheme}\n<style>\n${cssContent}\n</style>`,
-    },
-    { searchValue: '<!--diff2html-js-ui-->', replaceValue: `<script>\n${slimUIContent}\n</script>` },
-    { searchValue: '<!--diff2html-line-diff-->', replaceValue: lineByLineHTMLContent },
-    { searchValue: '<!--diff2html-side-diff-->', replaceValue: sideBySideHTMLContent },
-].reduce(
-    (previousValue, replacement) =>
-        replaceExactly(previousValue, replacement.searchValue, replacement.replaceValue),
-    template
-);
+const { lineByLineHTMLContent, sideBySideHTMLContent } = generateDiffHTMLContent(diffString);
+const modifiedTemplate = replacePlaceholdersInTemplate(pageTitle, versionsHTMLContent, lineByLineHTMLContent, sideBySideHTMLContent);
 
-fs.writeFileSync("diff.html", result);
+writeFileSync("diff.html", modifiedTemplate);
+
+function replaceExactly(value, searchValue, replaceValue) {
+    return value.replace(searchValue, () => replaceValue);
+}
+
+function readDiffString() {
+    return readFileSync("git.diff", "utf8");
+}
+
+function generateDiffHTMLContent(diffString, colorScheme = 'auto') {
+    const lineByLineParsed = parse(diffString, { outputFormat: "line-by-line", colorScheme });
+    const lineByLineHTMLContent = html(lineByLineParsed, { outputFormat: "line-by-line", colorScheme });
+
+    const sideBySideParsed = parse(diffString, { outputFormat: "side-by-side", colorScheme });
+    const sideBySideHTMLContent = html(sideBySideParsed, { outputFormat: "side-by-side", colorScheme });
+    return { lineByLineHTMLContent, sideBySideHTMLContent };
+}
+
+function replacePlaceholdersInTemplate(pageTitle, versionsHTMLContent, lineByLineHTMLContent, sideBySideHTMLContent) {
+    const template = readFileSync("template.html", "utf8");
+    const cssContent = readFileSync(path.resolve(__dirname, "node_modules/diff2html/bundles/css/diff2html.min.css"), "utf8");
+    const slimUIContent = readFileSync(path.resolve(__dirname, "node_modules/diff2html/bundles/js/diff2html-ui-slim.min.js"), "utf8");
+
+    const modifiedTemplate = [
+        { searchValue: '<!--diff2html-title-->', replaceValue: pageTitle },
+        { searchValue: '<!--diff2html-header-->', replaceValue: pageTitle },
+        { searchValue: '<!--diff2html-versions-->', replaceValue: versionsHTMLContent },
+        {
+            searchValue: '<!--diff2html-css-->',
+            replaceValue: `${autoBaseStyle}\n${autoGitHubTheme}\n<style>\n${cssContent}\n</style>`,
+        },
+        { searchValue: '<!--diff2html-js-ui-->', replaceValue: `<script>\n${slimUIContent}\n</script>` },
+        { searchValue: '<!--diff2html-line-diff-->', replaceValue: lineByLineHTMLContent },
+        { searchValue: '<!--diff2html-side-diff-->', replaceValue: sideBySideHTMLContent },
+    ].reduce(
+        (previousValue, replacement) =>
+            replaceExactly(previousValue, replacement.searchValue, replacement.replaceValue),
+        template
+    );
+
+    return modifiedTemplate;
+}
--- styles.js
+++ styles.js
@@ -1,26 +1,6 @@
 
-const lightGitHubTheme = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css" />`;
-const darkGitHubTheme = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css" />`;
 const autoGitHubTheme = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css" media="screen and (prefers-color-scheme: light)" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css" media="screen and (prefers-color-scheme: dark)" />`;
-
-const lightBaseStyle = `<style>
-body {
-  background-color: var(--d2h-bg-color);
-}
-h1 {
-  color: var(--d2h-light-color);
-}
-</style>`;
-
-const darkBaseStyle = `<style>
-body {
-  background-color: rgb(13, 17, 23);
-}
-h1 {
-  color: var(--d2h-dark-color);
-}
-</style>`;
 
 const autoBaseStyle = `<style>
 @media screen and (prefers-color-scheme: light) {
@@ -42,10 +22,6 @@ const autoBaseStyle = `<style>
 </style>`;
 
 export {
-    lightGitHubTheme,
-    darkGitHubTheme,
     autoGitHubTheme,
-    lightBaseStyle,
-    darkBaseStyle,
     autoBaseStyle,
 }
